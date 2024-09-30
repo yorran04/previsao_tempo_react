db22b7fbb8b4e365c71f6117b60cb8ac3354fb60
yorran nascimento <yorran_alves04@hotmail.com>
2024-09-30 16:59:39 -0300

    projeto previsao tempo

--- previsao-tempo/src/App.jsx
+++ previsao-tempo/src/App.jsx
@@ -7,7 +7,8 @@ import Weatherinfo5days from './Components/Weatherinfo5days/Info5days'
 
 
 function App() {
-  const [weather, setWeather] = useState({})
+  const [weather, setWeather] = useState()
+  const [weather5Days, setWeather5Days] = useState()
   const inputRef = useRef() 
 
   async function SeachCity (){
@@ -15,10 +16,14 @@ function App() {
     const city = inputRef.current.value
     const key = "a7ea8755b78910311cd1705b397d9e64"
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
+    const url5days = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&lang=pt_br&units=metric`
     
     const apiData = await axios.get(url)
+    const apiInfo5days = await axios.get(url5days)
 
     setWeather(apiData.data)
+    setWeather5Days(apiInfo5days.data)
+    
   }
 
   return (
@@ -28,9 +33,8 @@ function App() {
       <input ref={inputRef} type='text' placeholder='Digite um cidade'/>
       <button onClick={SeachCity}>Buscar</button>
       </div>
-     <Weatherinfo dados={weather}/>
-     <Weatherinfo5days/>
-      
+    {weather && < Weatherinfo dados={weather}/>}
+    {weather5Days && < Weatherinfo5days weather5Days={weather5Days}/>}
     </div>
   )
 }
--- previsao-tempo/src/Components/Weatherinfo5days/Info5days.jsx
+++ previsao-tempo/src/Components/Weatherinfo5days/Info5days.jsx
@@ -1,18 +1,42 @@
+/* eslint-disable react/prop-types */
 import './Info5days.css'
 
-function Weatherinfo5days() {
-  return (
-    <div className='container'>
-        <h3 className='ola'>Previsão do tempo para os próximos 5 dias </h3>
-        <div className='item-content'> 
-            <div className="item"></div>
-            <div className="item"></div>
-            <div className="item"></div>
-            <div className="item"></div>
-            <div className="item"></div>
-        </div>
-    </div>
-  );
-}
+// eslint-disable-next-line react/prop-types
+function Weatherinfo5days({weather5Days}) {
+  
+  let dailyForecast = {}
 
+  for (let forecast of weather5Days.list) {
+      const date = new Date(forecast.dt * 1000).toLocaleDateString()
+
+      if (!dailyForecast[date]) {
+          dailyForecast[date] = forecast
+      }
+  }
+  const next5DaysForecast = Object.values(dailyForecast).slice(1, 6)
+
+  function convertDate(date) {
+      const newDate = new Date(date.dt * 1000).toLocaleDateString('pt-BR', { weekday: 'long', day: '2-digit' })
+
+      return newDate
+  }
+  return (
+      <div className='container'>
+          <h3 className=''>Previsão Próximos 5 Dias</h3>
+          <div className='weather-list'>
+              {next5DaysForecast.map(forecast => (
+                  <div key={forecast.dt} className='weather-item'>
+                      <p className='forecast-day'>{convertDate(forecast)}</p>
+                      <img
+                          src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`}
+                          alt="Weather icon"
+                      />
+                      <p className='forecast-description'>{forecast.weather[0].description}</p>
+                      <p>{Math.round(forecast.main.temp_min)}ºC min /{Math.round(forecast.main.temp_max)}ºC máx</p>
+                  </div>
+              ))}
+          </div>
+      </div>
+  )
+}
 export default Weatherinfo5days
